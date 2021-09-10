f366d1ac2b858d919da2ad2a3230b0b805ae09bc
topser9 <topser181@gmail.com>
2021-09-11 02:34:46 +0700

    exynos9610: Allow to overclock the CPU
    Signed-off-by: topser9 <topser181@gmail.com>

--- drivers/cpufreq/exynos-acme.h
+++ drivers/cpufreq/exynos-acme.h
@@ -58,6 +58,10 @@ struct exynos_cpufreq_domain {
 
 	unsigned int			max_freq;
 	unsigned int			min_freq;
+#ifdef CONFIG_ARM_MODCLOCK
+	unsigned int			max_usable_freq;
+	unsigned int			min_usable_freq;
+#endif
 	unsigned int			boot_freq;
 	unsigned int			resume_freq;
 	unsigned int			old;
